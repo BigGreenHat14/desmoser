b91197733cd7ef5731cc0f4dbf23a972c3b6204b
BigGreenHat14 <163580586+BigGreenHat14@users.noreply.github.com>
2025-02-27 20:45:19 -0600

    Update desmoser_test.py

--- desmoser_test.py
+++ desmoser_test.py
@@ -4,4 +4,4 @@ import math
 obj = match_case(var("op"),[
     a + b,a - b,a * b,a / b,a ** b,a % b,a // b,a.nth_root()
 ])
-print(obj.convert_to_expression(var("calc"),var("res")))
+print(obj.convert_to_expression(var("calc"),var("res"))) # calc = res -> ...
